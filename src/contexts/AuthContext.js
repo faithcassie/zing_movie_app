e743f4ecd26064f1356b8bedf278f13055959c86
Faith Nguyen <faithc.nguyen@gmail.com>
2023-03-01 15:56:38 -0600

    third api fix

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -3,12 +3,14 @@ import React, { createContext, useEffect, useState } from "react";
 export const authContext = createContext();
 
 function AuthContext({ children }) {
-  const api = {
-    key: process.env.REACT_APP_APIKEY,
-    base: "https://api.themoviedb.org/3/",
-  };
+  // const api = {
+  //   key: process.env.REACT_APP_APIKEY,
+  //   base: "https://api.themoviedb.org/3/",
+  // };
 
-  const value = { api };
+  // const value = { api };
+  const movieTitle = "Mahogany teakwood";
+  const value = { movieTitle };
   return <authContext.Provider value={value}>{children}</authContext.Provider>;
 }
 
